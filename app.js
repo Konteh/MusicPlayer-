7c68badbef4f04963dab5f5b4d03abe87702a657
Konteh <kontehebrima993@gmail.com>
2021-08-19 11:34:45 -0700

    navigation added

---
+++ app.js
@@ -0,0 +1,70 @@
+///// carousels/////////////////
+
+const carousel = [...document.querySelectorAll('.carousel img')];
+
+let carouselImageIndex = 0;
+
+const changeCarousel = () => {
+    carousel[carouselImageIndex].classList.toggle('active');
+
+    if(carouselImageIndex >= carousel.length - 1){
+        carouselImageIndex = 0;
+    } else{
+        carouselImageIndex++;
+    }
+
+    carousel[carouselImageIndex].classList.toggle('active');
+}
+
+setInterval(() => {
+    changeCarousel();
+}, 3000);
+
+
+/////////////////////navigations////////////
+
+////////////toggling music player
+
+const musicPlayerSection = document.querySelector('.music-player-section');
+
+let clickCount = 1;
+
+musicPlayerSection.addEventListener('click', () => {
+    // checking for double click manually idk why default dbclick event was not working with this project If you know what could the problem Kindly tell me in the discussion below
+    if(clickCount >= 2){
+        musicPlayerSection.classList.add('active');
+        clickCount = 1;
+        return;
+    }
+    clickCount++;
+    setTimeout(() => {
+        clickCount = 1;
+    }, 250);
+})
+
+/////// back from music player
+
+const backToHomeBtn = document.querySelector('.music-player-section .back-btn');
+
+backToHomeBtn.addEventListener('click', () => {
+    musicPlayerSection.classList.remove('active');
+})
+
+//////// access playlist
+
+const playlistSection = document.querySelector('.playlist');
+const navBtn = document.querySelector('.music-player-section .nav-btn');
+
+navBtn.addEventListener('click', () => {
+    playlistSection.classList.add('active');
+})
+
+////////// back from playlist to music player
+
+const backToMusicPlayer = document.querySelector('.playlist .back-btn');
+
+backToMusicPlayer.addEventListener('click', () => {
+    playlistSection.classList.remove('active');
+})
+
+//////navigation done ////////////////
