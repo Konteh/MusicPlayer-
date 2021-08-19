f28b7e7a4857179ae6fe2d4161eab97d5ac7461e
Konteh <kontehebrima993@gmail.com>
2021-08-19 12:25:57 -0700

    add all music data in the data js file and update the music files in the html

--- data.js
+++ data.js
@@ -11,5 +11,41 @@ let songs = [
         artist: 'artist 2',
         cover: 'assets/images/cover 2.png'
     },
-    // +6 more
+    {
+        name: 'song 3',
+        path: 'assets/musics/Song 3.mp3',
+        artist: 'artist 3',
+        cover: 'assets/images/cover 3.png'
+    },
+    {
+        name: 'song 4',
+        path: 'assets/musics/Song 4.mp3',
+        artist: 'artist 4',
+        cover: 'assets/images/cover 4.png'
+    },
+    {
+        name: 'song 5',
+        path: 'assets/musics/Song 5.mp3',
+        artist: 'artist 5',
+        cover: 'assets/images/cover 5.png'
+    },
+    {
+        name: 'song 6',
+        path: 'assets/musics/Song 6.mp3',
+        artist: 'artist 6',
+        cover: 'assets/images/cover 6.png'
+    },
+    {
+        name: 'song 7',
+        path: 'assets/musics/Song 7.mp3',
+        artist: 'artist 7',
+        cover: 'assets/images/cover 7.png'
+    },
+    {
+        name: 'song 8',
+        path: 'assets/musics/Song 8.mp3',
+        artist: 'artist 8',
+        cover: 'assets/images/cover 8.png'
+    },
+    
 ]
